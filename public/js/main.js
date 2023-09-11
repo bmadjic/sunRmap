f0c6ffdf3da9fab3b935e109480aa2955f02d5ce
Madjic <benjamin.dupuy95@gmail.com>
2023-09-11 11:18:21 +0200

    refacto()

--- public/js/main.js
+++ public/js/main.js
@@ -1,103 +1,38 @@
-// Creating the layers per project type
-const carportGroup = L.layerGroup();
-const floatingGroup = L.layerGroup();
-const groundGroup = L.layerGroup();
-const rooftopGroup = L.layerGroup();
-const unknownGroup = L.layerGroup();
-
-const countriesGroup = L.layerGroup();
-
-// Map 
+// Initialisation de la carte
 var map = L.map('map', {
   center: [51.505, -0.09],
-  zoom: [2],
-  layers: [carportGroup, floatingGroup, groundGroup, rooftopGroup, unknownGroup]
+  zoom: 2,
 });
 
+// Ajout du fond de carte
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Configuration des icônes
+const iconsConfig = {
+  'Carport': '/images/blue.png',
+  'Floating': '/images/green.png',
+  'Ground': '/images/yellow.png',
+  'Rooftop': '/images/violet.png',
+  'Unknown': '/images/grey.png'
+};
 
-
-// Icons
-var carportIcon = L.icon({
-  //iconUrl: '/images/carport.png',
-  iconUrl: '/images/blue.png',
-  iconSize: [25, 41],
-  iconAnchor: [25, 50],
-  popupAnchor: [0, -50]
-});
-
-var floatingIcon = L.icon({
-  //iconUrl: '/images/floating.png',
-  iconUrl: '/images/green.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [0, -50]
-});
-
-var groundIcon = L.icon({
-  //iconUrl: '/images/ground.png',
-  iconUrl: '/images/yellow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [0, -50]
-});
-
-var rooftopIcon = L.icon({
-  //iconUrl: '/images/rooftop.png',
-  iconUrl: '/images/violet.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [0, -50]
-});
-
-
-var unknownIcon = L.icon({
-  //iconUrl: '/images/pin.svg',
-  iconUrl: '/images/grey.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [0, -50]
-});
-
-
-// Function to make the API call and return the data
-async function fetchApiData() {
-  try {
-    const response = await fetch('/api/data');
-    const data = await response.json();
-    //return data.results;
-    return data.results.filter(item => item.properties.pipeline == 52295361);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return null;
-  }
+function createIcon(url) {
+  return L.icon({
+    iconUrl: url,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [0, -50]
+  });
 }
 
-// Function to display the API data in a debug format
-async function displayApiResult(results) {
-  if (results && Array.isArray(results)) {
-    let output = '<h2>Data from API:</h2><ol>';
-    results.forEach((item) => {
-      output += `
-      <li>
-      Deal Name: ${item.properties.dealname || 'N/A'}, 
-      Latitude: ${item.properties.latitude || 'N/A'}, 
-      Longitude: ${item.properties.longitude || 'N/A'}, 
-      Country: ${item.properties.pays || 'N/A'},
-      Project Type: ${item.properties.type_of_project__pv_ || 'N/A'},
-      Amount: ${item.properties.amount || 'N/A'},
-      Pipeline : ${item.properties.pipeline || 'N/A'}
-      </li>`;
-    });
-    output += '</ol>';
-    document.getElementById('api-data').innerHTML = output;
-  }
+const icons = {};
+for (const [type, url] of Object.entries(iconsConfig)) {
+  icons[type] = createIcon(url);
 }
 
-// Function to add pins to the map
+// Groupes de calques par type de projet
 const projectTypeClusterGroups = {
   'Carport': L.layerGroup().addTo(map),
   'Floating': L.layerGroup().addTo(map),
@@ -105,7 +40,51 @@ const projectTypeClusterGroups = {
   'Rooftop': L.layerGroup().addTo(map),
   'Unknown': L.layerGroup().addTo(map)
 };
+
 const countryTypeClusterGroups = {};
+const countriesGroup = L.layerGroup();
+
+// Fonctions auxiliaires
+async function fetchApiData() {
+  try {
+    const response = await fetch('/api/data');
+    const data = await response.json();
+    return data.results.filter(item => item.properties.pipeline == 52295361);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
+function displayApiResult(results) {
+  if (results && Array.isArray(results)) {
+    let output = '<h2>Data from API:</h2><ol>';
+    results.forEach((item) => {
+      output += `<li>Deal Name: ${item.properties.dealname || 'N/A'}, Latitude: ${item.properties.latitude || 'N/A'}, Longitude: ${item.properties.longitude || 'N/A'}, Country: ${item.properties.pays || 'N/A'}, Project Type: ${item.properties.type_of_project__pv_ || 'N/A'}, Amount: ${item.properties.amount || 'N/A'}, Pipeline: ${item.properties.pipeline || 'N/A'}</li>`;
+    });
+    output += '</ol>';
+    document.getElementById('api-data').innerHTML = output;
+  }
+}
+
+function createMarker(item, selectedIcon, latitude, longitude) {
+  return L.marker([latitude, longitude], { icon: selectedIcon })
+    .bindPopup(`<strong>${item.properties.dealname || 'N/A'}</strong><br>
+    <strong>Power:</strong> ${item.properties.amount || 'N/A'} MWp<br>
+    <strong>Country:</strong> ${item.properties.pays || 'N/A'}<br>
+    <strong>Project Type:</strong> ${item.properties.type_of_project__pv_ || 'Unknown'}`);
+}
+
+function addToCluster(marker, projectType, country) {
+  const clusterKey = `${country}-${projectType}`;
+  if (!countryTypeClusterGroups[clusterKey]) {
+    countryTypeClusterGroups[clusterKey] = L.markerClusterGroup().addTo(map);
+  }
+  countryTypeClusterGroups[clusterKey].addLayer(marker);
+  if (projectTypeClusterGroups[projectType]) {
+    projectTypeClusterGroups[projectType].addLayer(countryTypeClusterGroups[clusterKey]);
+  }
+}
 
 function addPinsToMap(results) {
   results.forEach((item) => {
@@ -115,42 +94,9 @@ function addPinsToMap(results) {
     const country = item.properties.pays || 'Unknown';
     
     if (!isNaN(latitude) && !isNaN(longitude)) {
-      let selectedIcon;
-      switch (projectType.toLowerCase()) {
-        case 'carport':
-          selectedIcon = carportIcon;
-          break;
-        case 'floating':
-          selectedIcon = floatingIcon;
-          break;
-        case 'ground':
-          selectedIcon = groundIcon;
-          break;
-        case 'rooftop':
-          selectedIcon = rooftopIcon;
-          break;
-        default:
-          selectedIcon = unknownIcon;
-      }
-      
-      const marker = L.marker([latitude, longitude], { icon: selectedIcon })
-      .bindPopup(`
-      <strong>${item.properties.dealname || 'N/A'}</strong> <br>
-      <strong>Power:</strong> ${item.properties.amount || 'N/A'} MWp<br>
-      <strong>Country:</strong> ${item.properties.pays || 'N/A'}<br>
-      <strong>Project Type:</strong> ${projectType}
-      `);
-      
-      const clusterKey = `${country}-${projectType}`;
-      
-      if (!countryTypeClusterGroups[clusterKey]) {
-        countryTypeClusterGroups[clusterKey] = L.markerClusterGroup().addTo(map);
-      }
-
-      countryTypeClusterGroups[clusterKey].addLayer(marker);
-      if (projectTypeClusterGroups[projectType]) {
-        projectTypeClusterGroups[projectType].addLayer(countryTypeClusterGroups[clusterKey]);
-      }
+      const selectedIcon = icons[projectType] || icons['Unknown'];
+      const marker = createMarker(item, selectedIcon, latitude, longitude);
+      addToCluster(marker, projectType, country);
     }
   });
 }
--- utils/createIcon.js
+++
@@ -1,5 +0,0 @@
-// Utility function to create icons. Import this wherever you need to use it.
-const createIcon = (typeOfProject) => {
-    // Your logic to create icons based on typeOfProject
-  };
-  
